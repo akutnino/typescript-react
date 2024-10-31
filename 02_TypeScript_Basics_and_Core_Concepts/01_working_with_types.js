76376a3af236adc10aa21038c5260261fafb6d57
Nino Akut <akutnino@gmail.com>
2024-11-01 01:06:00 +0800

    Basic Primitive Types

--- 02_TypeScript_Basics_and_Core_Concepts/01_working_with_types.js
+++ 02_TypeScript_Basics_and_Core_Concepts/01_working_with_types.js
@@ -6,4 +6,7 @@ var firstName = 'Nino';
 var age = 23;
 var isMale = true;
 var bigNum = 9123412999999999993412341234n;
-console.log(isMale);
+var nulls = null;
+var undefine = undefined;
+var mySymbol = Symbol(23);
+console.log(typeof mySymbol);
--- 02_TypeScript_Basics_and_Core_Concepts/01_working_with_types.ts
+++ 02_TypeScript_Basics_and_Core_Concepts/01_working_with_types.ts
@@ -1,3 +1,13 @@
 // Inference & Explicit Type Annotations
 let userName: string = 'Nino';
 console.log(userName);
+
+// Basic Primitive Types
+const firstName: string = 'Nino';
+const age: number = 23;
+const isMale: boolean = true;
+const bigNum: bigint = 9123412999999999993412341234n;
+const nulls: null = null;
+const undefine: undefined = undefined;
+const mySymbol: symbol = Symbol(23);
+console.log(mySymbol);
