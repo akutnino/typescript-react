323b5876716d6d19ecb50a8801e1621ad71236b5
Nino Akut <akutnino@gmail.com>
2024-11-05 04:47:31 +0800

    Another Way Of Typing Components - FC Generics way

--- 03_Using_TypeScript_with_React_-_Essentials/react-ts-basics/src/components/CourseGoal-v1.tsx
+++ 03_Using_TypeScript_with_React_-_Essentials/react-ts-basics/src/components/CourseGoal-v1.tsx
@@ -1,16 +1,13 @@
-import { type ReactNode } from 'react';
+import { type FC, type PropsWithChildren } from 'react';
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
 // interface CourseGoalProps {
 // 	title: string;
 // 	children: ReactNode;
 // }
 
-type CourseGoalProps = {
-	title: string;
-	children: ReactNode;
-};
-
-function CourseGoal(props: CourseGoalProps) {
+const CourseGoal: FC<CourseGoalProps> = (props) => {
 	const { title, children } = props;
 
 	return (
@@ -22,6 +19,6 @@ function CourseGoal(props: CourseGoalProps) {
 			<button type='button'>Delete</button>
 		</article>
 	);
-}
+};
 
 export default CourseGoal;
