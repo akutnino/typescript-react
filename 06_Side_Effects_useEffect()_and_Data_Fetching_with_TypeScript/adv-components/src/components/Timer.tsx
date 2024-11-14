a1e2426db8885cb7b7731c4dc09861f447cf2567
Nino Akut <akutnino@gmail.com>
2024-11-15 00:44:51 +0800

    Creating a First Side Effect

--- 06_Side_Effects_useEffect()_and_Data_Fetching_with_TypeScript/adv-components/src/components/Timer.tsx
+++ 06_Side_Effects_useEffect()_and_Data_Fetching_with_TypeScript/adv-components/src/components/Timer.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from './ui/Container.tsx';
 import { Timer as TimerProps } from '../stores/timers-context.tsx';
 
@@ -8,11 +9,22 @@ import { Timer as TimerProps } from '../stores/timers-context.tsx';
 
 export default function Timer(props: TimerProps) {
 	const { name, duration } = props;
+	const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+	setInterval(() => {
+		setRemainingTime((currentTime) => currentTime - 1000);
+	}, 1000);
 
 	return (
 		<Container as='article'>
 			<h2>{name}</h2>
-			<p>{duration}</p>
+			<p>
+				<progress
+					max={duration * 1000}
+					value={remainingTime}
+				/>
+			</p>
+			<p>{}</p>
 		</Container>
 	);
 }
