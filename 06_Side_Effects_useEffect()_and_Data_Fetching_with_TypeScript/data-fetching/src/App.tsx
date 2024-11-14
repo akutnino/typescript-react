5bb7499b8e2ce862f39b18f0d5c0092e8d3eb464
Nino Akut <akutnino@gmail.com>
2024-11-15 05:27:55 +0800

    Fetching & Transforming Data

--- 06_Side_Effects_useEffect()_and_Data_Fetching_with_TypeScript/data-fetching/src/App.tsx
+++ 06_Side_Effects_useEffect()_and_Data_Fetching_with_TypeScript/data-fetching/src/App.tsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
+import { get } from './utils/http.ts';
+import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
+import fetchImg from './assets/data-fetching.png';
+
+type RawDataBlogPost = {
+	id: number;
+	userId: number;
+	title: string;
+	body: string;
+};
+
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts';
+
 function App() {
-	return <h1>Data Fetching!</h1>;
+	const [fetchedPosts, setFetchedPost] = useState<BlogPost[] | []>([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const data = (await get(fetchURL)) as RawDataBlogPost[];
+
+			const blogPosts: BlogPost[] = data.map((postObject) => {
+				const blogPostObject: BlogPost = {
+					id: postObject.id,
+					title: postObject.title,
+					text: postObject.body,
+				};
+
+				return blogPostObject;
+			});
+
+			setFetchedPost((currentPosts) => [...currentPosts, ...blogPosts]);
+		};
+
+		fetchPosts();
+		return () => {};
+	}, []);
+
+	return (
+		<main>
+			<img
+				src={fetchImg}
+				alt='fetching image'
+			/>
+			<BlogPosts posts={fetchedPosts} />
+		</main>
+	);
 }
 
 export default App;
--- 06_Side_Effects_useEffect()_and_Data_Fetching_with_TypeScript/data-fetching/src/utils/http.ts
+++ 06_Side_Effects_useEffect()_and_Data_Fetching_with_TypeScript/data-fetching/src/utils/http.ts
@@ -9,6 +9,7 @@ export async function get(url: string) {
 	const response = await fetch(url, fetchOptions);
 	if (!response.ok) throw new Error('FETCH REQUEST FAILED.');
 
-	const data = response.json() as unknown;
+	const data = (await response.json()) as unknown;
+
 	return data;
 }
