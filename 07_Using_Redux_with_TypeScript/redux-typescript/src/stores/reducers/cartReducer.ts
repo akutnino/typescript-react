b93fde51730fb8c890a09fe6e869f0bea021890d
Nino Akut <akutnino@gmail.com>
2024-11-17 04:27:23 +0800

    Setting a State Type

--- 07_Using_Redux_with_TypeScript/redux-typescript/src/stores/reducers/cartReducer.ts
+++ 07_Using_Redux_with_TypeScript/redux-typescript/src/stores/reducers/cartReducer.ts
@@ -1,3 +1,32 @@
-const INITIAL_STATE = {};
+type CartItem = {
+	id: string;
+	title: string;
+	price: number;
+	quantity: number;
+};
 
-function cartReducer() {}
+type CartState = {
+	items: CartItem[];
+};
+
+type ActionType = {
+	type: string;
+	payload: any;
+};
+
+const INITIAL_STATE: CartState = {
+	items: [],
+};
+
+function cartReducer(currentState = INITIAL_STATE, action: ActionType) {
+	switch (action.type) {
+		case '': {
+			return;
+		}
+
+		default:
+			return currentState;
+	}
+}
+
+export default cartReducer;
